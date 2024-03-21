a9ebcf0f0872cb5aec05f368094931a93efb7e19
YardenNezah <74673812+YardenNezah@users.noreply.github.com>
2024-03-21 22:01:10 +0200

    build settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "netlify-deployments": {
+      enabled: true,
+      config: {
+        accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+        sites: [
+          {
+            name: 'Site 1',
+            id: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+            buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+            branch: 'main' // optional
+          }
+        ]
+      },
+    },
+  });
+  
+
+
+
