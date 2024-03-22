1f96d814bed227747b03846966344a9946a29cc5
YardenNezah <74673812+YardenNezah@users.noreply.github.com>
2024-03-22 10:31:09 +0200

    modify index- without admin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "netlify-deployments": {
+      enabled: true,
+      config: {
+        accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+        sites: [
+          {
+            name: 'wordtune',
+            id: "2d016cdc-2889-47d4-8a54-10819816ee0c",
+            buildHook: "https://api.netlify.com/build_hooks/65fd2e3438fbe9dd5e7a6915",
+            branch: 'main' // optional
+          }
+        ]
+      },
+    },
+  });
+  
+
+
+
