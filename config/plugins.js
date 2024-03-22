530d41ceffd9f8265ce45c300bd9bf34821af6cc
YardenNezah <74673812+YardenNezah@users.noreply.github.com>
2024-03-22 09:11:23 +0200

    settings

--- config/plugins.js
+++ config/plugins.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
         accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
         sites: [
           {
-            name: 'Site 1',
-            id: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-            buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+            name: 'wordtune',
+            id: "2d016cdc-2889-47d4-8a54-10819816ee0c",
+            buildHook: "https://api.netlify.com/build_hooks/65fd2e3438fbe9dd5e7a6915",
             branch: 'main' // optional
           }
         ]
